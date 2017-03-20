8a0d0ced0940050967488b151b929bd003c28192
veroandi <veroandi.wcs@gmail.com>
2017-03-20 22:58:46 +0100

    search bar w/ .css created

--- page/search.php
+++ page/search.php
@@ -1,7 +1,24 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Alex
- * Date: 20/03/2017
- * Time: 14:12
- */
+<!doctype html>
+<html>
+<head>
+    <link href="../public/CSS/styleSearchBar.css" rel='stylesheet'>
+    <title>Search Bar</title>
+</head>
+<body>
+
+
+<div class="container-fluid">
+    <div class="row">
+        <form class="searchbar" action="search.php" method="GET">
+            <input type="text" class="form-control" name="query" placeholder="Chercher par garage, concessionaire etc" />
+        </form>
+
+        <form class="searchbar" action="search.php" method="GET">
+            <input type="text" class="form-control" name="query" placeholder="Chercher par ville ou code postale" />
+            <button type="submit" class="btn btn-default">OK</button>
+        </form>
+    </div>
+</div>
+
+</body>
+</html>
