c95ee682abdba4ac3e335ecd0e894adb8df595ad
amor bounechada <amor.bounechada@gmail.com>
2017-03-21 17:56:04 +0100

    add navbar

--- inc/header.php
+++ inc/header.php
@@ -1,7 +1,57 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Alex
- * Date: 20/03/2017
- * Time: 14:07
- */
+<head>
+    <link rel="stylesheet" href="/public/CSS/main.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap Core CSS -->
+    <link href="/public/CSS/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="/public/CSS/scrolling-nav.css" rel="stylesheet">
+</head>
+<!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top " role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top"><img src="/img/logo.png" width="200" height="30"></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a class="page-scroll" href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll navy" href="#">Inscription</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#">Connexion</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#">Professionnels</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- jQuery -->
+    <script src="/public/js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="/public/js/bootstrap.min.js"></script>
+    <!-- Scrolling Nav JavaScript -->
+    <script src="/public/js/jquery.easing.min.js"></script>
+    <script src="/public/js/scrolling-nav.js"></script>
+
+</body>
+
+
+
