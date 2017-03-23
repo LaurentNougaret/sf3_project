41c71f7125c8b39fc782dc805d2cfe4a45d898d6
veroandi <veroandi.wcs@gmail.com>
2017-03-23 10:52:50 +0100

    search bar & page of results finished for the 1st sprint

--- page/search.php
+++ page/search.php
@@ -1,26 +1,13 @@
-<!doctype html>
-<html>
-<head>
-    <link href="../public/CSS/styleSearchBar.css" rel='stylesheet'>
-    <link href="../public/CSS/bootstrap.css" rel='stylesheet'>
-    <title>Barre de Recherche</title>
-</head>
-<body>
-
-
-<div class="container-fluid">
+<div id="SearchBar" class="container-fluid">
     <div class="row">
-        <form class="searchbar" action="search.php" method="GET">
+        <div><form class="searchbar form-inline" action="searchResults.php" method="GET">
             <input type="text" class="form-control" name="query" placeholder="Chercher par professionnel " />
         </form>
-
-        <form class="searchbar" action="search.php" method="GET">
+        </div>
+        <div><form class="searchbar" action="searchResults.php" method="GET">
             <input type="text" class="form-control" name="query" placeholder="Chercher par ville ou code postale" />
-            <button type="submit" class="btn btn-default">OK</button>
         </form>
+        </div>
+        <div><button type="submit" class="btn btn-default">OK</button></div>
     </div>
 </div>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-</body>
-</html>
--- public/index.php
+++ public/index.php
@@ -54,6 +54,8 @@ $file = $page . ".php";
         crossorigin="anonymous"></script>
 <!-- Ajoutez vos liens vers dossiers js ici -->
 <script type="text/javascript" src="js/"></script>
+<!-- JQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
 </body>
 </html>
