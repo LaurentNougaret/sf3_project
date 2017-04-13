24ce92d714fbb6733ac94a19a74161f334e7ee85
veroandi <veroandi.wcs@gmail.com>
2017-04-13 15:23:43 +0200

    creation of contact page

--- page/contact.php
+++ page/contact.php
@@ -1,7 +1,52 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: diesel
- * Date: 20/03/17
- * Time: 17:17
- */
+<div class="container">
+<form action="" method="post" role="form" class="form-horizontal formContact">
+    <fieldset>
+            <div class="bloc">
+                <h2 class="text-center">Contactez-nous</h2>
+                <hr>
+                <!-- Text area - Last name -->
+                <div class="form-group">
+                    <label for="" class="col-sm-3 control-label">Nom</label>
+                    <div class="col-sm-6">
+                        <input class="form-control" name="surname" id="surname" placeholder="Nom" type="text">
+                    </div>
+                </div>
+                <!-- Text area - First name -->
+                <div class="form-group">
+                    <label for="" class="col-sm-3 control-label">Prenom</label>
+                    <div class="col-sm-6">
+                        <input class="form-control" name="firstname" id="firstname" placeholder="Prénom" type="text">
+                    </div>
+                </div>
+                <!-- Text area - E-mail -->
+                <div class="form-group">
+                    <label for="" class="col-sm-3 control-label">E-mail</label>
+                    <div class="col-sm-6">
+                        <input class="form-control" name="email" id="email" placeholder="E-mail" type="email">
+                    </div>
+                </div>
+                <!-- Text area - Title of message -->
+                <div class="form-group">
+                    <label for="" class="col-sm-3 control-label">Titre</label>
+                    <div class="col-sm-6">
+                        <input class="form-control" name="title" id="title" placeholder="Titre" type="text">
+                    </div>
+                </div>
+                <!-- Text area - Write message -->
+                <div class="form-group">
+                    <label for="" class="col-sm-3 control-label">Message</label>
+                    <div class="col-sm-6">
+                        <textarea class="form-control" id="contactForm" name="contactForm" rows="8" placeholder="Laissez votre message"></textarea>
+                    </div>
+                </div>
+                <!-- Button - Save review -->
+                <div class="form-group">
+                    <label class="col-md-5 control-label" for="saveReviewButton"></label>
+                    <div class="col-md-4">
+                        <button id="sendMessage" name="sendMessage" class="btn btn-primary">Envoyer</button>
+                    </div>
+                </div>
+            </div>
+    </fieldset>
+</form>
+</div>
