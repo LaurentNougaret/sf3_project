30d9f0a8f3f99dcca8d3720c2d5fa831e263a5aa
veroandi <veroandi.wcs@gmail.com>
2017-04-18 17:25:51 +0200

    contact form

--- page/contact.php
+++ page/contact.php
@@ -1,51 +1,68 @@
-<div class="container">
-<form action="" method="post" role="form" class="form-horizontal formContact">
-    <fieldset>
-            <div class="bloc">
-                <h2 class="text-center">Contactez-nous</h2>
-                <hr>
-                <div class="form-group">
-                    <label for="" class="col-sm-3 control-label">Nom</label>
-                    <div class="col-sm-6">
-                        <input class="form-control" name="surname" id="surname" placeholder="Nom" type="text">
+<div class="container contactPage">
+    <div class="row">
+        <h2 class="text-center">Contactez-nous</h2>
+        <hr>
+        <h4 class="contactWelcomePhrase"> Vous avez des questions, des suggestions ou des demandes? Envoyez-nous un message!</h4>
+        <div class="col-xs-7">
+            <form action="" method="post" role="form" class="form-horizontal formContact">
+                <fieldset>
+                    <div class="form-group">
+                        <label for="surname" class="col-sm-3 control-label">Nom</label>
+                        <div class="col-sm-6">
+                            <input class="form-control" name="surname" id="surname" placeholder="Nom" type="text">
+                        </div>
                     </div>
-                </div>
-                <!-- Text area - Title of message -->
-                <div class="form-group">
-                    <label for="" class="col-sm-3 control-label">Prenom</label>
-                    <div class="col-sm-3">
-                        <input class="form-control" name="name" id="name" placeholder="Prénom" type="text">
+                    <!-- Text area - Title of message -->
+                    <div class="form-group">
+                        <label for="name" class="col-sm-3 control-label">Prénom</label>
+                        <div class="col-sm-6">
+                            <input class="form-control" name="name" id="name" placeholder="Prénom" type="text">
+                        </div>
                     </div>
-                </div>
-                <!-- Text area - E-mail -->
-                <div class="form-group">
-                    <label for="" class="col-sm-3 control-label">E-mail</label>
-                    <div class="col-sm-3">
-                        <input class="form-control" name="email" id="email" placeholder="" type="email">
+                    <!-- Text area - E-mail -->
+                    <div class="form-group">
+                        <label for="email" class="col-sm-3 control-label">E-mail</label>
+                        <div class="col-sm-6">
+                            <input class="form-control" name="email" id="email" placeholder="E-mail" type="email">
+                        </div>
                     </div>
-                </div>
-                <!-- Text area - Title of message -->
-                <div class="form-group">
-                    <label for="" class="col-sm-3 control-label">Titre</label>
-                    <div class="col-sm-6">
-                        <input class="form-control" name="title" id="title" placeholder="Titre" type="text">
+                    <!-- Text area - Subject of the message -->
+                    <div class="form-group">
+                        <label for="subject" class="col-sm-3 control-label">Objet</label>
+                        <div class="col-sm-6">
+                            <input class="form-control" name="subject" id="subject" placeholder="Objet" type="text">
+                        </div>
                     </div>
-                </div>
-                <!-- Text area - Write message -->
-                <div class="form-group">
-                    <label for="" class="col-sm-3 control-label">Message</label>
-                    <div class="col-sm-6">
-                        <textarea class="form-control" id="contactForm" name="contactForm" rows="8" placeholder="Laissez votre message"></textarea>
+                    <!-- Text area - Write message -->
+                    <div class="form-group">
+                        <label for="contactForm" class="col-sm-3 control-label">Message</label>
+                        <div class="col-sm-6">
+                            <textarea class="form-control" id="contactForm" name="contactForm" rows="8" placeholder="Laissez votre message"></textarea>
+                        </div>
                     </div>
-                </div>
-                <!-- Button - Save review -->
-                <div class="form-group">
-                    <label class="col-md-5 control-label" for="saveReviewButton"></label>
-                    <div class="col-md-4">
-                        <button id="sendMessage" name="sendMessage" class="btn btn-primary">Envoyer</button>
+                    <!-- Button - Save review -->
+                    <div class="form-group">
+                        <label for="sendMessageButton" class="col-sm-6 control-label"></label>
+                        <div class="col-sm-6">
+                            <button id="sendMessageButton" name="sendMessageButton" class="btn sendMessageButton">Envoyer</button>
+                        </div>
                     </div>
-                </div>
+                </fieldset>
+            </form>
+        </div>
+        <div class="col-xs-4 ">
+            <div class="formContactDivAddress">
+                <h4> CarAdvisor </h4>
+                <address>
+                    Siège social
+                    <br> C.A.
+                    <br>
+                    11, Avenue XYW
+                    <br>69800 LYON</address>
+                <br>
+                <p>Tel: 01.02.03.04.05</p>
+                <p>E-mail: <a href="mailto:contact@caradvisor.com">contact@caradvisor.com</a></p>
             </div>
-    </fieldset>
-</form>
+        </div>
+    </div>
 </div>
