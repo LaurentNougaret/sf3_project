c691a8ce530f499f78443be3e48c6e58924ec121
veroandi <veroandi.wcs@gmail.com>
2017-04-19 10:54:45 +0200

    finish contact

--- page/contact.php
+++ page/contact.php
@@ -3,54 +3,42 @@
         <h2 class="text-center">Contactez-nous</h2>
         <hr>
         <h4 class="contactWelcomePhrase"> Vous avez des questions, des suggestions ou des demandes? Envoyez-nous un message!</h4>
-        <div class="col-xs-7">
+        <div class="col-xs-10 col-xs-offset-1 col-md-6 offset-md-3">
             <form action="" method="post" role="form" class="form-horizontal formContact">
                 <fieldset>
                     <div class="form-group">
-                        <label for="surname" class="col-sm-3 control-label">Nom</label>
-                        <div class="col-sm-6">
+                        <label for="surname">Nom</label>
                             <input class="form-control" name="surname" id="surname" placeholder="Nom" type="text">
-                        </div>
                     </div>
                     <!-- Text area - Title of message -->
                     <div class="form-group">
-                        <label for="name" class="col-sm-3 control-label">Prénom</label>
-                        <div class="col-sm-6">
+                        <label for="name">Prénom</label>
                             <input class="form-control" name="name" id="name" placeholder="Prénom" type="text">
-                        </div>
                     </div>
                     <!-- Text area - E-mail -->
                     <div class="form-group">
-                        <label for="email" class="col-sm-3 control-label">E-mail</label>
-                        <div class="col-sm-6">
+                        <label for="email">E-mail</label>
                             <input class="form-control" name="email" id="email" placeholder="E-mail" type="email">
-                        </div>
                     </div>
                     <!-- Text area - Subject of the message -->
                     <div class="form-group">
-                        <label for="subject" class="col-sm-3 control-label">Objet</label>
-                        <div class="col-sm-6">
+                        <label for="subject">Objet</label>
                             <input class="form-control" name="subject" id="subject" placeholder="Objet" type="text">
-                        </div>
                     </div>
                     <!-- Text area - Write message -->
                     <div class="form-group">
-                        <label for="contactForm" class="col-sm-3 control-label">Message</label>
-                        <div class="col-sm-6">
+                        <label for="contactForm">Message</label>
                             <textarea class="form-control" id="contactForm" name="contactForm" rows="8" placeholder="Laissez votre message"></textarea>
-                        </div>
                     </div>
                     <!-- Button - Save review -->
                     <div class="form-group">
-                        <label for="sendMessageButton" class="col-sm-6 control-label"></label>
-                        <div class="col-sm-6">
-                            <button id="sendMessageButton" name="sendMessageButton" class="btn sendMessageButton">Envoyer</button>
-                        </div>
+                        <label for="sendMessageButton"></label>
+                            <button id="sendMessageButton" name="sendMessageButton" class="btn sendMessageButton center-block">Envoyer</button>
                     </div>
                 </fieldset>
             </form>
         </div>
-        <div class="col-xs-4 ">
+        <div class="col-xs-12 col-md-3">
             <div class="formContactDivAddress">
                 <h4> CarAdvisor </h4>
                 <address>
