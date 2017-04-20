65e6c6d1220d5090f429f76c197b49ddbd3c9e52
LaurentNougaret <uriel3@hotmail.fr>
2017-04-20 15:35:19 +0200

    Merged devAmor

--- config/ConfigDistrib.php
+++ config/ConfigDistrib.php
@@ -5,8 +5,8 @@ namespace caradvisor;
 // Put in here your credentials for connection to mySQL
 class Config
 {
-    const HOST = "localhost";
-    const USER = "root";
-    const PASSWORD = "toto";
-    const DBNAME = "caradvisor";
+    const HOST = "";
+    const USER = "";
+    const PASSWORD = "";
+    const DBNAME = "";
 }
