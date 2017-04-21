d79c335e8edd393953a40fdd77cc6bc694f5a93d
veroandi <veroandi.wcs@gmail.com>
2017-04-21 10:32:19 +0200

    changetext alert about cookies

--- public/js/cookieconsent.js
+++ public/js/cookieconsent.js
@@ -12,7 +12,8 @@ window.addEventListener("load", function(){
         },
         "theme": "classic",
         "content": {
-            "message": "En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de [Cookies ou autres traceurs ]pour vous proposer [Par exemple, des publicités ciblées adaptés à vos centres d’intérêts] et [Par exemple, réaliser des statistiques de visites].",
+            "message": "En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de traceurs " +
+            "(cookies) pour vous proposer des contenus et services adaptés à vos centres d'intérêts.",
             "dismiss": "Ok, tout accepter",
             "link": "En savoir plus",
             "href": "/?page=legal"
