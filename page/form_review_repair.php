23e8ef345e22a40c1e7fb4a1f144b525a3bfaca4
veroandi <veroandi.wcs@gmail.com>
2017-04-25 10:12:39 +0200

    calendar-datepicker

--- page/form_review_repair.php
+++ page/form_review_repair.php
@@ -83,48 +83,12 @@
                             <textarea class="form-control" id="reviewBox" name="reviewBox" rows="7" placeholder="Décrivez votre experience..."></textarea>
                         </div>
                     </div>
+                    <!-- Date input -->
                     <div class="form-group">
-                        <label for="" class="col-xs-12 col-sm-3 control-label">Date de la prise en charge</label>
-                        <div class="col-xs-3 col-sm-1">
-                            <select name="appointment" id="appointment"">
-                                <option value="default" selected>Jour</option>
-                                <?php
-                                for ($menuday = 1; $menuday <= 31; $menuday++) {
-                                    ?>
-                                    <option value="<?php echo $menuday; ?>">
-                                        <?php echo $menuday; ?>
-                                    </option>
-                                    <?php
-                                }
-                                ?>
-                            </select>
-                        </div>
-                        <div class="col-xs-3 col-sm-1">
-                            <select name="MonthOfAppointment">
-                                <option value="default" selected>Mois</option>
-                                <?php
-                                for ($menuday = 1; $menuday <= 12; $menuday++) { ?>
-                                    <option value="<?php echo $menuday; ?>">
-                                        <?php echo $menuday; ?>
-                                    </option>
-                                    <?php
-                                }
-                                ?>
-                            </select>
-                        </div>
-                        <div class="col-xs-3 col-sm-1">
-                            <select name="YearOfAppointment">
-                                <option value="default" selected>Année</option>
-                                <?php
-                                for ($menuyear = date('Y'); $menuyear >= 1900; $menuyear--) {
-                                    ?>
-                                    <option value="<?php echo $menuyear; ?>">
-                                        <?php echo $menuyear; ?>
-                                    </option>
-                                    <?php
-                                }
-                                ?>
-                            </select>
+                        <label for="date" class="col-xs-12 col-sm-3 control-label">Date de l'achat</label>
+                        <div class="col-sm-2 input-group" id="sandbox-container">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
+                            <input class="form-control" id="date" name="date" placeholder="dd/mm/aaaa" type="text">
                         </div>
                     </div>
                 </div>
