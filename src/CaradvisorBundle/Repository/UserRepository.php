683a7bbc9a4f3b76786b93da7b388187142d58cf
veroandi <veroandi.wcs@gmail.com>
2017-06-19 17:10:47 +0200

    getreviews slider

--- src/CaradvisorBundle/Repository/UserRepository.php
+++ src/CaradvisorBundle/Repository/UserRepository.php
@@ -6,20 +6,4 @@ use Doctrine\ORM\EntityRepository;
 
 class UserRepository extends EntityRepository
 {
-    /**
-     * @param int $user_id
-     * @return array
-     */
-    public function getReviewUser($user_id)
-    {
-        $qb = $this->createQueryBuilder('u')
-                    ->where('u.id = :user_id')
-                    ->setParameter('user_id', $user_id)
-                    ->join('u.reviewBuys','rb')
-                    ->join('u.reviewRepairs', 'rr')
-                    ->select('u, rb ,rr')
-                    ->getQuery();
-        return $qb->getResult();
-    }
-
 }
