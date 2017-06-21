aa9391502ebda83babc6ff90da2a02b1d225fee2
veroandi <veroandi.wcs@gmail.com>
2017-06-21 14:13:09 +0200

    start admin section

--- src/CaradvisorBundle/Controller/AdminController.php
+++ src/CaradvisorBundle/Controller/AdminController.php
@@ -4,15 +4,31 @@ namespace CaradvisorBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends Controller
 {
     /**
      * @Route ("/admin", name="admin")
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param Request $request
+     * @return Response
      */
-    public function indexAction()
+    public function loginAction(Request $request)
     {
-        return $this->render('@Caradvisor/Admin/index.html.twig');
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        //get error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        //last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('@Caradvisor/Admin/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
+
+
 }
